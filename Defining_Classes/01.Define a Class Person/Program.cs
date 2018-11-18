bd720033f5145e2f66f5fce1a0f2a0dc67a92073
matanovm <matanov.martin@gmail.com>
2018-11-19 00:35:07 +0200

    Add Ex: 1-5

---
+++ Defining_Classes/01.Define a Class Person/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Define_a_Class_Person
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
---
+++ Defining_Classes/03.Oldest Family Member/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Oldest_Family_Member
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
