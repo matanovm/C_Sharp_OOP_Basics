acc4152f7d7ba4de7b4cabc56ff9af96a538b807
matanovm <matanov.martin@gmail.com>
2018-12-18 02:50:58 +0200

    Add ex 2,3,4,5,6,7

--- Working With Abstraction/02. Car Salesman/Program.cs
+++ Working With Abstraction/02. Car Salesman/Program.cs
@@ -6,10 +6,80 @@ using System.Threading.Tasks;
 
 namespace _02.Car_Salesman
 {
-	class Program
+	public class CarSalesman
 	{
 		static void Main(string[] args)
 		{
+			List<Car> cars = new List<Car>();
+			List<Engine> engines = new List<Engine>();
+			int engineCount = int.Parse(Console.ReadLine());
+
+			for (int i = 0; i < engineCount; i++)
+			{
+				string input = Console.ReadLine();
+				string[] parameters = input.Split(' ');
+
+				string model = parameters[0];
+				int power = int.Parse(parameters[1]);
+				int displacement = -1;
+				bool parseparameters = int.TryParse(parameters[2], out displacement);
+
+				if (parameters.Length == 3 && parseparameters)
+				{
+					engines.Add(new Engine(model, power, displacement));
+				}
+				else if (parameters.Length == 3)
+				{
+					string efficiency = parameters[2];
+					engines.Add(new Engine(model, power, efficiency));
+				}
+				else if (parameters.Length == 4)
+				{
+					string efficiency = parameters[3];
+					engines.Add(new Engine(model, power, int.Parse(parameters[2]), efficiency));
+				}
+				else
+				{
+					engines.Add(new Engine(model, power));
+				}
+			}
+
+			int carCount = int.Parse(Console.ReadLine());
+
+			for (int i = 0; i < carCount; i++)
+			{
+				string[] parameters = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+				string model = parameters[0];
+				string engineModel = parameters[1];
+				Engine engine = engines.FirstOrDefault(x => x._model == engineModel);
+				int weight = -1;
+				bool parameter = int.TryParse(parameters[2], out weight);
+
+				if (parameters.Length == 3 && parameter)
+				{
+					cars.Add(new Car(model, engine, weight));
+				}
+				else if (parameters.Length == 3)
+				{
+					string color = parameters[2];
+					cars.Add(new Car(model, engine, color));
+				}
+				else if (parameters.Length == 4)
+				{
+					string color = parameters[3];
+					cars.Add(new Car(model, engine, int.Parse(parameters[2]), color));
+				}
+				else
+				{
+					cars.Add(new Car(model, engine));
+				}
+			}
+
+			foreach (Car car in cars)
+			{
+				Console.WriteLine(car);
+			}
 		}
+
 	}
 }
--- Working With Abstraction/03. Jed Galaxy/Program.cs
+++ Working With Abstraction/03. Jed Galaxy/Program.cs
@@ -10,6 +10,65 @@ namespace _03.Jed_Galaxy
 	{
 		static void Main(string[] args)
 		{
+			string[] dim = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+			System.Collections.Generic.IEnumerable<int> dim2 = dim.Select(int.Parse);
+			int[] dimentions = dim2.ToArray();
+			int x = dimentions[0];
+			int y = dimentions[1];
+			int[,] matrix = new int[x, y];
+			int value = 0;
+
+			for (int i = 0; i < x; i++)
+			{
+				for (int j = 0; j < y; j++)
+				{
+					matrix[i, j] = value++;
+				}
+			}
+
+			string command = Console.ReadLine();
+			long sum = 0;
+
+			while (command != "Let the Force be with you")
+			{
+				string[] ivoSector = command.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+				System.Collections.Generic.IEnumerable<int> ivoSector2 = ivoSector.Select(int.Parse);
+				int[] ivoS = ivoSector2.ToArray();
+				string[] evilPosition = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+				var evilPosition2 = evilPosition.Select(int.Parse);
+				int[] evil = evilPosition2.ToArray();
+				int xEvil = evil[0];
+				int yEvil = evil[1];
+
+				while (xEvil >= 0 && yEvil >= 0)
+				{
+					if (xEvil >= 0 && xEvil < matrix.GetLength(0) && yEvil >= 0 && yEvil < matrix.GetLength(1))
+					{
+						matrix[xEvil, yEvil] = 0;
+					}
+
+					xEvil--;
+					yEvil--;
+				}
+
+				int xIvos = ivoS[0];
+				int yIvos = ivoS[1];
+
+				while (xIvos >= 0 && yIvos < matrix.GetLength(1))
+				{
+					if (xIvos >= 0 && xIvos < matrix.GetLength(0) && yIvos >= 0 && yIvos < matrix.GetLength(1))
+					{
+						sum += matrix[xIvos, yIvos];
+					}
+
+					yIvos++;
+					xIvos--;
+				}
+
+				command = Console.ReadLine();
+			}
+
+			Console.WriteLine(sum);
 		}
 	}
 }
