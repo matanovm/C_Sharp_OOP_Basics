af395e9a2215046912c870b229c4da3f487d1053
matanovm <matanov.martin@gmail.com>
2018-12-18 02:51:39 +0200

    Update Ex 2,7

---
+++ Working With Abstraction/02. Car Salesman/Engine.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Car_Salesman
+{
+	public class Engine
+	{
+		private const string offset = "  ";
+
+		public string _model;
+		public int _power;
+		public int _displacement;
+		public string _efficiency;
+
+		public Engine(string model, int power)
+		{
+			_model = model;
+			_power = power;
+			_displacement = -1;
+			_efficiency = "n/a";
+		}
+
+		public Engine(string model, int power, int displacement)
+		{
+			_model = model;
+			_power = power;
+			_displacement = displacement;
+			_efficiency = "n/a";
+		}
+
+		public Engine(string model, int power, string efficiency)
+		{
+			_model = model;
+			_power = power;
+			_displacement = -1;
+			_efficiency = efficiency;
+		}
+
+		public Engine(string model, int power, int displacement, string efficiency)
+		{
+			_model = model;
+			_power = power;
+			_displacement = displacement;
+			_efficiency = efficiency;
+		}
+
+		public override string ToString()
+		{
+			StringBuilder sb = new StringBuilder();
+			sb.AppendFormat("{0}{1}:\n", offset, this._model);
+			sb.AppendFormat("{0}{0}Power: {1}\n", offset, this._power);
+			sb.AppendFormat("{0}{0}Displacement: {1}\n", offset, this._displacement == -1 ? "n/a" : this._displacement
+				.ToString());
+			sb.AppendFormat("{0}{0}Efficiency: {1}\n", offset, this._efficiency);
+
+			return sb.ToString();
+		}
+	}
+}
---
+++ Working With Abstraction/07. Family Tree/Person.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.Family_Tree
+{
+	public class Person
+	{
+		private string _name;
+		private string _birthday;
+		private List<Person> _parents;
+		private List<Person> _children;
+
+		public Person()
+		{
+			_children = new List<Person>();
+			Parents = new List<Person>();
+		}
+
+		public Person(string name)
+			: this()
+		{
+			_name = name;
+		}
+
+		public string FullName
+		{
+			get { return _name; }
+			set { _name = value; }
+		}
+
+		public string Birthday
+		{
+			get { return _birthday; }
+			set { _birthday = value; }
+		}
+
+		public List<Person> Parents
+		{
+			get { return _parents; }
+			set { _parents = value; }
+		}
+
+		public List<Person> Children
+		{
+			get { return _children; }
+			set { _children = value; }
+		}
+
+		public static Person CreatePerson(string personInput)
+		{
+			Person person = new Person();
+
+			if (IsBirthday(personInput))
+			{
+				person.Birthday = personInput;
+			}
+			else
+			{
+				person.FullName = personInput;
+			}
+
+			return person;
+		}
+
+		private static bool IsBirthday(string input)
+		{
+			return char.IsDigit(input[0]);
+		}
+
+		public override string ToString()
+		{
+			return $"{FullName} {Birthday}";
+		}
+	}
+}
